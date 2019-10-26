2969117b6426a19f8c7427961a17bce52a68d40b
pc <gitstream007>
2019-10-26 17:30:35 +0200

    basics of this symfony/vuejs project

---
+++ assets/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <salut></salut>
+    </div>
+</template>
+
+<script>
+    import Hello from './components/Hello'
+
+    class TestClassSyntax {
+
+    }
+
+    export default {
+        name: 'app'
+        components: {
+            Hello
+        }
+    }
+</script>
---
+++ assets/js/Components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Hello.vue",
+        data() {
+            return {
+                msg: 'Salut salut'
+            }
+        }
+    }
+</script>
